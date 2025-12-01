84c7d027421b5ff34778d506ed115eb5ac369ff3
Richie Jefts <rjefts@gmail.com>
2025-11-30 22:35:19 -0600

    Setup 2025

---
+++ aoc-2025/src/day01.rs
@@ -0,0 +1,28 @@
+use aoc_runner_derive::{aoc};
+
+#[aoc(day1, part1)]
+fn part1(input: &str) -> u32 {
+    11
+}
+
+#[aoc(day1, part2)]
+fn part2(input: &str) -> u32 {
+    31
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    static INPUT1: &str = "";
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(INPUT1), 11);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(INPUT1), 31)
+    }
+}
